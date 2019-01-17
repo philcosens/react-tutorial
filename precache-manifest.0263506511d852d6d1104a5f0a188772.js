7cb07ce773052b5584b48beb78d4486d86f67203
Phillip Cosens <phillip.cosens@shopdirect.com>
2019-01-17 11:46:10 +0000

    Updates

---
+++ precache-manifest.0263506511d852d6d1104a5f0a188772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7708c55329eff165e599",
+    "url": "/philcosens/react-tutorial/static/js/runtime~main.7708c553.js"
+  },
+  {
+    "revision": "5121130fbb0e7257992b",
+    "url": "/philcosens/react-tutorial/static/js/main.5121130f.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/philcosens/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "5121130fbb0e7257992b",
+    "url": "/philcosens/react-tutorial/static/css/main.6d9044a8.chunk.css"
+  },
+  {
+    "revision": "b3fabc70e48d772db5c1f7192aedcfc1",
+    "url": "/philcosens/react-tutorial/index.html"
+  }
+];
